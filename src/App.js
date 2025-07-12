792c78f3e76a0ec65e7ad3545bd3d9e743b79979
Gabi Saito <gabrielasaitopereira@email.com>
2025-07-12 20:04:23 -0300

    chore: criação de rotas e estrutura inicial de pastas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter } from 'react-router-dom';
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
+import Join from './Pages/Join';
+import Members from './Pages/Members';
+import Events from './Pages/Events';
+import NotFoundPage from './Pages/NotFoundPage';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <> 
+    <CssBaseline>
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/sobre-nos' element={<Members />} />
+          <Route path='/eventos' element={<Events />} />
+          <Route path='/participe' element={<Join />} />
+          <Route path='/contato' element={<Contact />} />
+          <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+      </BrowserRouter>
+    </CssBaseline>
+  </>
   );
 }
 
